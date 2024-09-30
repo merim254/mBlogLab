e2d2ad2b8c596e2be7723508f0175ce136529a6e
Merima Mohammed <merimmohammed.mm@gmail.com>
2024-09-30 11:53:15 +0300

    Initial project commit

---
+++ BlogLab.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogLab.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
